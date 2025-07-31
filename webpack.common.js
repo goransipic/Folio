1dcaff9351624f51c2159d178e541ab2a9a4ff27
goransipic <goran.sipic@certilia.com>
2025-07-31 09:49:18 +0200

    new images and 404.html

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './js/app.js',
+    app: './assets/js/functions.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
